7c61debe494db92174444d4b4f170fb0e627be97
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-01-05 01:24:58 -0800

    Format

--- nextjs-flask/app/page.tsx
+++ nextjs-flask/app/page.tsx
@@ -78,66 +78,68 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen text-center">
-		<div className="flex-grow py-64">
-      <h1 className="shrikhand-regular">Vibe Check</h1>
-      <div className="card">
-        {!submitted ? (
-          <>
-            <h2 className="bold">Share how you're feeling! — {context}</h2>
+      <div className="flex-grow py-64">
+        <h1 className="shrikhand-regular">Vibe Check</h1>
+        <div className="card">
+          {!submitted ? (
+            <>
+              <h2 className="bold">Share how you're feeling! — {context}</h2>
 
-            {find_event(context) ? (
-              <>
-                <div className="stars">
-                  {[1, 2, 3, 4, 5].map((star) => (
-                    <span
-                      key={star}
-                      className={
-                        star <= (hover || rating) ? "star selected" : "star"
-                      }
-                      onClick={() => setRating(star)}
-                      onMouseEnter={() => setHover(star)}
-                      onMouseLeave={() => setHover(rating)}
-                    >
-                      &#9733;
-                    </span>
-                  ))}
-                </div>
+              {find_event(context) ? (
+                <>
+                  <div className="stars">
+                    {[1, 2, 3, 4, 5].map((star) => (
+                      <span
+                        key={star}
+                        className={
+                          star <= (hover || rating) ? "star selected" : "star"
+                        }
+                        onClick={() => setRating(star)}
+                        onMouseEnter={() => setHover(star)}
+                        onMouseLeave={() => setHover(rating)}
+                      >
+                        &#9733;
+                      </span>
+                    ))}
+                  </div>
 
-                <textarea
-                  rows={2}
-                  className="auto_height"
-                  onInput={auto_height_func}
-                  placeholder="Leave a comment..."
-                  value={comment}
-                  onChange={(e) => setComment(e.target.value)}
-                />
+                  <textarea
+                    rows={2}
+                    className="auto_height"
+                    onInput={auto_height_func}
+                    placeholder="Leave a comment..."
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                  />
 
-                <br />
+                  <br />
 
-                <button className="submit-button" onClick={handleSubmit}>
-                  Submit
-                </button>
-              </>
-            ) : (
-              <>
-                <h2 className="bold">Event "{context}" not found.</h2>
-              </>
-            )}
-          </>
-        ) : (
-          <>
-            <h2 className="bold">Thanks for sharing! — {context}</h2>
+                  <button className="submit-button" onClick={handleSubmit}>
+                    Submit
+                  </button>
+                </>
+              ) : (
+                <>
+                  <h2 className="bold">Event "{context}" not found.</h2>
+                </>
+              )}
+            </>
+          ) : (
+            <>
+              <h2 className="bold">Thanks for sharing! — {context}</h2>
 
-            <button className="submit-button" onClick={handleSubmitAgain}>
-              Submit again
-            </button>
-          </>
-        )}
+              <button className="submit-button" onClick={handleSubmitAgain}>
+                Submit again
+              </button>
+            </>
+          )}
+        </div>
       </div>
+      <div className="w-full text-center mt-auto py-6">
+        <a href="https://forms.gle/K7KDopHZ47tCvARE7">
+          <h2 className="bold red-text">Feedback Form</h2>
+        </a>
       </div>
-		<div className="w-full text-center mt-auto py-6">
-			<a href="https://forms.gle/K7KDopHZ47tCvARE7"><h2 className="bold red-text">Feedback Form</h2></a>
-		</div>
     </div>
   );
 }
