c749b7169c2fcb11fe99ee5e87fca9024458858b
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-02-11 01:37:50 -0800

    Use empty string if it does not exist

--- nextjs-flask/app/page.tsx
+++ nextjs-flask/app/page.tsx
@@ -7,8 +7,8 @@ import "./index.css";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
 );
 
 export default function Home() {
